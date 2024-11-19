c07eee7384463d946bab41d5759d7e990b462a63
Filipe Mai <f.nunesmai19@gmail.com>
2024-11-19 11:50:34 -0300

    .

--- server/src/main/routes/inventory_routes.py
+++ server/src/main/routes/inventory_routes.py
@@ -2,6 +2,6 @@ from flask import Blueprint
 
 inventory_bp = Blueprint("inventory", __name__)
 
-@inventory_bp.route("/products/", methods=["GET"])
+@inventory_bp.route("/products", methods=["GET"])
 def find_all_products():
     pass
