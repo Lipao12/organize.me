54be39d3552a00aa086908dc5bfe0c1898eedaf6
Filipe Mai <f.nunesmai19@gmail.com>
2024-11-28 09:49:02 -0300

    .

--- client/src/products/modal_create_product.tsx
+++ client/src/products/modal_create_product.tsx
@@ -41,7 +41,7 @@ export const CreateProductModal = ({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
       <div className="bg-white p-6 rounded-lg w-96">
         <div className="flex items-center justify-between">
           <h2 className="text-lg font-semibold">Adicionar Novo Produto</h2>
