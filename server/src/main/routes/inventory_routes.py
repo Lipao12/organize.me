bb0ae59156ed597346d58b5da04f15a63257f44e
Filipe Mai <f.nunesmai19@gmail.com>
2024-11-28 09:46:38 -0300

    feat: registrar um novo usuário

--- server/src/main/routes/inventory_routes.py
+++ server/src/main/routes/inventory_routes.py
@@ -4,12 +4,34 @@ inventory_bp = Blueprint("inventory", __name__)
 
 # Imoportação de Controllers
 from src.controllers.product_finder import ProductsFinder
+from src.controllers.product_creator import ProductsCreator
+from src.controllers.user_creator import UserCreator 
 
 # Importação de Repositorios
 from src.models.repositories.products_repository import ProductsRepository
+from src.models.repositories.user_reposityory import UsersRepository 
 
 # Importação o gerente de conexões
 from src.models.settings.db_connection_handler import db_connection_handler
+
+@inventory_bp.route("/register", methods=["POST"])
+def create_user():
+    conn = db_connection_handler.get_connection()
+    users_repository = UsersRepository(conn)
+    controller = UserCreator(users_repository=users_repository)
+
+    response = controller.create(request.json)
+
+    return jsonify(response['body']), response['status_code']
+
+@inventory_bp.route("/products/create", methods=["POST"])
+def create_product():
+    conn = db_connection_handler.get_connection()
+    products_repository = ProductsRepository(conn)
+    controller = ProductsCreator(products_repository)
+
+    response = controller.create_new_product(request.json)
+    return jsonify(response['body']), response['status_code']
 
 @inventory_bp.route("/products", methods=["POST"])
 def find_products_paginated():
@@ -21,7 +43,7 @@ def find_products_paginated():
     return jsonify(response['body']), response['status_code']
 
 @inventory_bp.route("/products/all", methods=["POST"])
-def find_all_paginated():
+def find_all_products():
     conn = db_connection_handler.get_connection()
     products_repository = ProductsRepository(conn)
     controller = ProductsFinder(products_repository)
---
+++ server/src/models/repositories/user_reposityory.py
@@ -0,0 +1,57 @@
+from psycopg2.extensions import connection
+from typing import Dict, Tuple, Optional
+import bcrypt
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class UsersRepository:
+    def __init__(self, conn: connection) -> None:
+        self.conn = conn
+
+    def create_user(self, users_info: Dict[str, str]) -> None:
+        bcrypt_cost = int(os.getenv("BCRYPT_COST", os.getenv('BCRYPT_COST'))) 
+        salt = bcrypt.gensalt(rounds=bcrypt_cost)
+        hashed_password = bcrypt.hashpw(users_info["password"].encode('utf-8'), salt)
+        
+        cursor = self.conn.cursor()
+        cursor.execute(
+            '''
+            INSERT INTO Users
+                (id, name, email, password)
+            VALUES
+                (%s, %s, %s, %s)
+            ''',
+            (
+                users_info["id"],
+                users_info["name"],
+                users_info["email"],
+                hashed_password.decode('utf-8'),
+            )
+        )
+        self.conn.commit()
+
+    def find_user_by_id(self, user_id: str) -> Optional[Tuple]:
+        cursor = self.conn.cursor()
+        cursor.execute(
+            '''
+            SELECT id, name, email, created_at FROM Users WHERE id = %s
+            ''', 
+            (user_id,)
+        )
+        user = cursor.fetchone()
+        return user
+
+    def login(self, email: str, password: str) -> Optional[str]:
+        cursor = self.conn.cursor()
+        cursor.execute(
+            '''
+            SELECT id, password FROM Users WHERE email = %s
+            ''', 
+            (email,)
+        )
+        user = cursor.fetchone()
+        if user and bcrypt.checkpw(password.encode('utf-8'), user[1].encode('utf-8')):
+            return user[0]
+        return None
